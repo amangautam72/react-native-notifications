710de244feba20297f05ccb27d8246ab1972feab
amangautam72 <amangautam72@gmail.com>
2022-07-06 16:50:36 +0530

    Added callback for Killed State

--- lib/android/app/src/main/java/com/wix/reactnativenotifications/fcm/FcmInstanceIdListenerService.java
+++ lib/android/app/src/main/java/com/wix/reactnativenotifications/fcm/FcmInstanceIdListenerService.java
@@ -2,6 +2,8 @@ package com.wix.reactnativenotifications.fcm;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.content.SharedPreferences;
+import android.content.Context;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
@@ -25,6 +27,18 @@ public class FcmInstanceIdListenerService extends FirebaseMessagingService {
 
         try {
             final IPushNotification notification = PushNotification.get(getApplicationContext(), bundle);
+            SharedPreferences sharedPreferences = getApplicationContext().getSharedPreferences("MySharedPref",Context.MODE_PRIVATE);        
+            // Creating an Editor object to edit(write to the file)
+            SharedPreferences.Editor myEdit = sharedPreferences.edit();
+    
+            // Storing the key and its value as the data fetched from edittext
+            myEdit.putString("notification", String.valueOf(bundle));
+            String count = sharedPreferences.getString("notificationCount", "0");
+            myEdit.putString("notificationCount", String.valueOf(Integer.parseInt(count) + 1));
+            // Once the changes have been made,
+            // we need to commit to apply those changes made,
+            // otherwise, it will throw an error
+            myEdit.commit();
             notification.onReceived();
         } catch (IPushNotification.InvalidNotificationException e) {
             // An FCM message, yes - but not the kind we know how to work with.
